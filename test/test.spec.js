872c54a5a3b545e2dc39052f4e0d11ccf2a0f15e
Basavaraj K N <basavarajk@Basavarajs-MacBook-Pro.local>
2017-06-06 15:30:34 +0530

    Changes made to test the integration

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,16 +1,17 @@
-var should = require('should');
+let should = require('should');
 
 describe('First Test', function() {
   it('Sum of n consecutive numbers from 1 is n*(n+1)/2', function(done) {
     setTimeout(function() {
-      var n = Math.floor(Math.random()*100);
+      let n = Math.floor(Math.random()*100);
 
-      var count = 0;
-      for(var i=1; i<=n; i++) {
+      let count = 0;
+      for(let i=1; i<=n; i++) {
         count += i;
       }
 
       count.should.be.exactly(n*(n+1)/2);
+      count.should.not.equal(undefined);
       done();
     }, 300);
   });
